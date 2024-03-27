6e5110bfa2435b51639edb5e4775f9c3f2ecc1ee
Sora Schlegel <sorazora1.4@gmail.com>
2024-03-27 22:59:11 +0000

    changed conf file readings

--- Receiver/helpers/read_config.py
+++ Receiver/helpers/read_config.py
@@ -1,8 +1,21 @@
 import yaml, logging.config, logging
 
 
+def conf_filename(service):
+    if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
+        print("In Testing Environment")
+
+        conf_file = f"/config/{service}_conf.yml"
+    else:
+        conf_file = f"{service}_conf.yml"
+    
+    return conf_file
+
+
 def read_app_config():
-    with open('./config/app_conf.yml', 'r') as file:
+    app_conf_file = conf_filename('app')
+
+    with open(app_conf_file, 'r') as file:
         app_config = yaml.safe_load(file.read())
 
     return app_config
@@ -21,7 +34,9 @@ def get_kafka_config():
 
 
 def read_log_config():
-    with open('./config/log_conf.yml', 'r') as file:
+    log_conf_file = conf_filename('log')
+
+    with open(log_conf_file, 'r') as file:
         log_config = yaml.safe_load(file.read())
         logging.config.dictConfig(log_config)
 
--- Storage/helpers/read_config.py
+++ Storage/helpers/read_config.py
@@ -1,12 +1,24 @@
 import yaml, logging, logging.config
 
 
+def conf_filename(service):
+    if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
+        print("In Testing Environment")
+
+        conf_file = f"/config/{service}_conf.yml"
+    else:
+        conf_file = f"{service}_conf.yml"
+    
+    return conf_file
+
+
 def read_app_config():
-    with open('./config/app_conf.yml', 'r') as file:
+    app_conf_file = conf_filename('app')
+
+    with open(app_conf_file, 'r') as file:
         app_config = yaml.safe_load(file.read())
 
     return app_config
-
 
 def database_config():
     app_config = read_app_config()
@@ -27,7 +39,9 @@ def get_kafka_config():
     
 
 def read_log_config():
-    with open('./config/log_conf.yml', 'r') as file:
+    log_conf_file = conf_filename('log')
+
+    with open(log_conf_file, 'r') as file:
         log_config = yaml.safe_load(file.read())
         logging.config.dictConfig(log_config)
 
