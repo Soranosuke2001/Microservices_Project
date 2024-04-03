ff9718acf20698a40bf35a33304dae12b84ee3c3
Soranosuke2001 <sorazora1.4@gmail.com>
2024-04-02 21:29:40 -0700

    updated logging configs and reading config files

--- EventLogger/helpers/read_config.py
+++ EventLogger/helpers/read_config.py
@@ -1,8 +1,23 @@
-import yaml, logging.config, logging
+import os
+import yaml
+import logging.config
+import logging
+
+
+def conf_filename(service):
+    if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
+        print("In Testing Environment")
+
+        conf_file = f"/config/{service}_conf.yml"
+    else:
+        conf_file = f"{service}_conf.yml"
+    
+    return conf_file
 
 
 def read_app_config():
-    with open('./config/app_conf.yml', 'r') as file:
+    app_conf_file = conf_filename('app')
+    with open(app_conf_file, 'r') as file:
         app_config = yaml.safe_load(file.read())
 
     return app_config
@@ -38,7 +53,9 @@ def read_flask_config():
 
 
 def read_log_config():
-    with open('./config/log_conf.yml', 'r') as file:
+    log_conf_file = conf_filename('log')
+
+    with open(log_conf_file, 'r') as file:
         log_config = yaml.safe_load(file.read())
         logging.config.dictConfig(log_config)
 
